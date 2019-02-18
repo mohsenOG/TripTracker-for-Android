8b1b23595f999a5afee70d071defb9ab32eba4b7
wonderfulme <mohsen_og@yahoo.com>
2019-02-18 15:00:41 +0100

    [Bugfix] Route names list is sorted by DESC instead of ASC

--- app/src/main/java/eu/wonderfulme/triptracker/database/LocationDao.java
+++ app/src/main/java/eu/wonderfulme/triptracker/database/LocationDao.java
@@ -13,7 +13,7 @@ public interface LocationDao {
     @Insert
     void insertSingleRecord(LocationData data);
 
-    @Query("SELECT item_key, MIN(timestamp) as minTimestamp, route_name as routeName FROM LocationData GROUP BY item_key")
+    @Query("SELECT item_key, MIN(timestamp) as minTimestamp, route_name as routeName FROM LocationData GROUP BY item_key ORDER BY item_key DESC")
     LiveData<List<LocationHeaderData>> getAllLocationHeaderData();
 
     @Query("SELECT * FROM LocationData WHERE item_key = :itemKey")
